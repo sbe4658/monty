e806d432a66342cfe724251e08538c8dc46cfd13
Hissitissam <tissam.hisssi@gmail.com>
2024-01-17 00:48:38 -0800

    add and do nothing

---
+++ basic_opcodes1.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+ * _add - adds the top two elements of the stack
+ *
+ * @tophead:the head of the linked list
+ * @numline: numbers line;
+ * Return: return nothing
+ */
+
+void _add(stack_t **tophead, unsigned int numline)
+{
+	int cnt = 0;
+	stack_t *tempo = NULL;
+
+	tempo = *tophead;
+
+	for (; tempo != NULL; tempo = tempo->next, cnt++)
+		;
+
+	if (cnt < 2)
+	{
+		dprintf(2, "L%u: can't add, stack too short\n", numline);
+		exit(EXIT_FAILURE);
+	}
+
+	tempo = (*tophead)->next;
+	tempo->n += (*tophead)->n;
+	_pop(tophead, numline);
+}
+
+/**
+ * _nop - do anythinhg
+ *
+ * @tophead: the head of the linked list
+ * @numline: numbers line
+ * Return: return nothing
+ */
+
+void _nop(stack_t **tophead, unsigned int numline)
+{
+	(void)tophead;
+	(void)numline;
+}
+
